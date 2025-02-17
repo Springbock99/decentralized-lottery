5478947899b89deccd4f55f21310905aa8653f16
Springbock99 <daneelkent@gmail.com>
2025-02-17 09:58:44 +0100

    added function updateState()

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,64 +4,73 @@ pragma solidity ^0.8.25;
 // ownable2Step and erc-20/Ierc20;
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-contract Lottery is Ownable2Step{
+contract Lottery is Ownable2Step {
+    error InsufficientBalance();
+    error NotOpen();
+    error AmountZero();
 
-  error InsufficientBalance();
-  error NotOpen();
-  error AmountZero();
+    // constructor that inizializes a token or maybe not so many tokens could be used
+    // constructor ownable2 step
 
-// constructor that inizializes a token or maybe not so many tokens could be used
-// constructor ownable2 step
+    IERC20 public immutable token;
+    uint256 public ticketCounter;
+    uint256 public saleStartTime;
+    uint256 public saleEndTime;
 
-IERC20 immutable public token;
-uint256 public ticketCounter;
-uint256 public ticketsToIssue = 100;
+    mapping(address => uint256[]) public ticketNumber;
+    mapping(uint256 => address) public ticketOwner;
 
-
-
-mapping(address => uint256[]) public ticketNumber;
-mapping(uint256 => address) public ticketOwner;
-
-enum LotteryState { TicketSaleOpen, LotteryClosed, CalculatingWinner }
-
-LotteryState public currentState;
-
-
-constructor(address _tokenAddress) Ownable(msg.sender){
-  token = IERC20(_tokenAddress);
-  currentState = LotteryState.TicketSaleOpen;
-}
-
-// functions
-
-function supplyToken(address _user, uint256 _amount) external {
-  //must have some safty checks. 
-  if(token.balanceOf(_user) < _amount) revert InsufficientBalance();
-  if(currentState != LotteryState.TicketSaleOpen ) revert NotOpen();
-  if (_amount == 0) revert AmountZero();
-  currentState = LotteryState.TicketSaleOpen;
-
-  token.transferFrom(msg.sender,address(this), _amount);
-
-
-  uint256 ticketsToIssue = _amount / 100;
-    
-    // Issue tickets by updating mappings.
-    for (uint256 i = 0; i < ticketsToIssue; i++) {
-        ticketCounter++;  // Generate a new unique ticket number.
-        ticketNumber[msg.sender].push(ticketCounter);  // Add the ticket to the user's list.
-        ticketOwner[ticketCounter] = msg.sender;         // Map the ticket number to the user.
+    enum LotteryState {
+        TicketSaleOpen,
+        LotteryClosed,
+        CalculatingWinner
     }
 
-}
+    LotteryState public currentState;
 
+    constructor(
+        address _tokenAddress,
+        uint256 _saleStartTime,
+        uint256 _saleEndTime
+    ) Ownable(msg.sender) {
+        token = IERC20(_tokenAddress);
+        currentState = LotteryState.TicketSaleOpen;
+        saleStartTime = _saleStartTime;
+        saleEndTime = _saleEndTime;
+    }
 
+    // functions
 
-function withdrawWinnings() external onlyOwner{
+    function updateState() internal {
+        if (
+            block.timestamp >= saleStartTime && block.timestamp <= saleEndTime
+        ) {
+            currentState = LotteryState.TicketSaleOpen;
+        } else {
+            currentState = LotteryState.LotteryClosed;
+        }
+    }
 
-}
+    function supplyToken(address _user, uint256 _amount) external {
+        updateState();
+        if (token.balanceOf(_user) < _amount) revert InsufficientBalance();
+        if (currentState != LotteryState.TicketSaleOpen) revert NotOpen();
+        if (_amount == 0) revert AmountZero();
+        currentState = LotteryState.TicketSaleOpen;
 
-function checkLotteryNr() external view returns(uint256) {}
+        token.transferFrom(msg.sender, address(this), _amount);
 
+        uint256 ticketsToIssue = _amount / 100;
 
+        // Issue tickets by updating mappings.
+        for (uint256 i = 0; i < ticketsToIssue; i++) {
+            ticketCounter++; // Generate a new unique ticket number.
+            ticketNumber[msg.sender].push(ticketCounter); // Add the ticket to the user's list.
+            ticketOwner[ticketCounter] = msg.sender; // Map the ticket number to the user.
+        }
+    }
+
+    function withdrawWinnings() external onlyOwner {}
+
+    function checkLotteryNr() external view returns (uint256) {}
 }
