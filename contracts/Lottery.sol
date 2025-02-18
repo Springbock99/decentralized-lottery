dad19b5d0083698fa5e9b6a02255ea394de07ebc
Springbock99 <daneelkent@gmail.com>
2025-02-18 23:18:55 +0100

    adding changes to the lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,21 +4,37 @@ pragma solidity ^0.8.25;
 // ownable2Step and erc-20/Ierc20;
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-contract Lottery is Ownable2Step {
+import {VRFV2PlusWrapperConsumerBase} from "@chainlink-contracts-1.3.0/vrf/dev/VRFV2PlusWrapperConsumerBase.sol";
+import {VRFV2PlusClient} from "@chainlink-contracts-1.3.0/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {LinkTokenInterface} from "@chainlink-contracts-1.3.0/shared/interfaces/LinkTokenInterface.sol";
+
+contract Lottery is Ownable2Step, VRFV2PlusWrapperConsumerBase {
     error InsufficientBalance();
     error NotOpen();
     error AmountZero();
+    error LotteryActive();
+    error NotTheWinner();
 
     // constructor that inizializes a token or maybe not so many tokens could be used
     // constructor ownable2 step
 
     IERC20 public immutable token;
+    address public winner;
+
     uint256 public ticketCounter;
     uint256 public saleStartTime;
     uint256 public saleEndTime;
+    uint32 public callbackGasLimit = 100000;
+    uint16 public requestConfirmations = 3;
+    uint32 public numWords = 1;
+
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
 
     mapping(address => uint256[]) public ticketNumber;
     mapping(uint256 => address) public ticketOwner;
+    mapping(uint256 => RequestStatus)
+        public s_requests; /* requestId --> requestStatus */
 
     enum LotteryState {
         TicketSaleOpen,
@@ -26,17 +42,58 @@ contract Lottery is Ownable2Step {
         CalculatingWinner
     }
 
+    struct RequestStatus {
+        uint256 paid; // amount paid in link
+        bool fulfilled; // whether the request has been successfully fulfilled
+        uint256[] randomWords;
+    }
+
     LotteryState public currentState;
 
     constructor(
         address _tokenAddress,
         uint256 _saleStartTime,
-        uint256 _saleEndTime
-    ) Ownable(msg.sender) {
+        uint256 _saleEndTime,
+        address _vrfV2PlusWrapper
+    ) VRFV2PlusWrapperConsumerBase(_vrfV2PlusWrapper) Ownable(msg.sender) {
         token = IERC20(_tokenAddress);
         currentState = LotteryState.TicketSaleOpen;
         saleStartTime = _saleStartTime;
         saleEndTime = _saleEndTime;
+    }
+
+    function requestRandomWords(
+        bool enableNativePayment
+    ) external onlyOwner returns (uint256) {
+        bytes memory extraArgs = VRFV2PlusClient._argsToBytes(
+            VRFV2PlusClient.ExtraArgsV1({nativePayment: enableNativePayment})
+        );
+        uint256 requestId;
+        uint256 reqPrice;
+        if (enableNativePayment) {
+            (requestId, reqPrice) = requestRandomnessPayInNative(
+                callbackGasLimit,
+                requestConfirmations,
+                numWords,
+                extraArgs
+            );
+        } else {
+            (requestId, reqPrice) = requestRandomness(
+                callbackGasLimit,
+                requestConfirmations,
+                numWords,
+                extraArgs
+            );
+        }
+        s_requests[requestId] = RequestStatus({
+            paid: reqPrice,
+            randomWords: new uint256[](0),
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+
+        return requestId;
     }
 
     // functions
@@ -70,7 +127,45 @@ contract Lottery is Ownable2Step {
         }
     }
 
-    function withdrawWinnings() external onlyOwner {}
+    function pickWinner() external onlyOwner {
+        bytes memory extra;
+        updateState();
+        if (currentState != LotteryState.LotteryClosed) revert LotteryActive();
 
-    function checkLotteryNr() external view returns (uint256) {}
+        requestRandomness(
+            callbackGasLimit,
+            requestConfirmations,
+            numWords,
+            extra
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 /* _requestId*/,
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 winningTicket = (randomWords[0] % ticketCounter) + 1;
+
+        winner = ticketOwner[winningTicket];
+    }
+
+    function withdrawWinnings() external onlyOwner {
+        updateState();
+        if (currentState != LotteryState.LotteryClosed) revert LotteryActive();
+        if (msg.sender != winner) revert NotTheWinner();
+        // calculate the winnigs.
+        // make sure the user is the one with the winning ticket
+        // transfer funds that are in the contract.
+        token.transferFrom(
+            address(this),
+            msg.sender,
+            token.balanceOf(address(this))
+        );
+        // double check if this function has to be only owner.
+        // variable of the winning nr.
+    }
+
+    function checkLotteryNr() external view returns (uint256) {
+        return ticketCounter;
+    }
 }
