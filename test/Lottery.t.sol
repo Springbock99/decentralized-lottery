a8cd5083c6fc6d109038d5c459d976599a0d7458
Springbock99 <daneelkent@gmail.com>
2025-02-24 11:01:02 +0100

    adding some more test.

--- test/Lottery.t.sol
+++ test/Lottery.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.25;
 import {Test, Vm, console} from "forge-std/Test.sol";
 import {Lottery} from "../contracts/Lottery.sol";
+import {IERC20} from "@openzeppelin-contracts-5.2.0/token/ERC20/IERC20.sol";
 
 contract LotteryTest is Test {
     Lottery public lottery;
@@ -9,6 +10,10 @@ contract LotteryTest is Test {
     address public owner;
     address public user1;
     address public user2;
+
+    address chainLinkToken = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+    address chainLinkVRFWrapper = 0x02aae1A04f9828517b3007f83f6181900CaD910c;
+    IERC20 link = IERC20(chainLinkToken);
 
     uint256 public constant TICKET_PRICE = 100;
     uint256 public constant INITIAL_BALANCE = 1000 * TICKET_PRICE;
@@ -19,8 +24,7 @@ contract LotteryTest is Test {
         user2 = makeAddr("user2");
 
         vm.createSelectFork(
-            "https://eth-mainnet.g.alchemy.com/v2/zNMGaVTfo_oc5Wngx6AQHIKUhcTXsCJD",
-            19_000_000
+            "https://eth-mainnet.g.alchemy.com/v2/zNMGaVTfo_oc5Wngx6AQHIKUhcTXsCJD"
         );
 
         vm.startPrank(owner);
@@ -29,34 +33,39 @@ contract LotteryTest is Test {
         uint256 startTime = block.timestamp;
         uint256 endTime = startTime + 1 days;
         lottery = new Lottery(
-            address(0x514910771AF9Ca656af840dff83E8264EcF986CA),
+            address(chainLinkToken),
             startTime,
             endTime,
-            address(0xD7f86b4b8Cae7D942340FF628F82735b7a20893a)
+            address(chainLinkVRFWrapper)
         );
 
         // // Mint tokens
         // token.mint(user1, INITIAL_BALANCE);
         // token.mint(user2, INITIAL_BALANCE);
 
-        deal(
-            address(0x514910771AF9Ca656af840dff83E8264EcF986CA),
-            user1,
-            1 ether
-        );
+        deal(address(link), user1, 1 ether);
 
+        uint256 balanceOfUser1 = IERC20(
+            0x514910771AF9Ca656af840dff83E8264EcF986CA
+        ).balanceOf(user1);
+
+        console.log("balance of user1", balanceOfUser1);
         // uint256 balanceUser1 = token.balanceOf(user1);
         // console.log("balance of user1:", balanceUser1);
 
         vm.stopPrank();
 
-        // vm.startPrank(user1);
-        // token.approve(address(this), INITIAL_BALANCE);
-        // vm.stopPrank();
+        vm.startPrank(user1);
+        link.approve(address(lottery), 1 ether);
+        console.log(
+            "Allowance for Lottery:",
+            link.allowance(user1, address(lottery))
+        );
+        vm.stopPrank();
 
-        // vm.startPrank(user2);
-        // token.approve(address(this), INITIAL_BALANCE);
-        // vm.stopPrank();
+        vm.startPrank(user2);
+        link.approve(address(this), 1 ether);
+        vm.stopPrank();
     }
 
     function test_InitialState() public {
@@ -71,21 +80,34 @@ contract LotteryTest is Test {
         );
     }
 
-    // function test_GetTicketCount() public {
-    //     uint256 BalanceOfUser1 = token.balanceOf(user1);
-    //     console.log("balance of user1:", BalanceOfUser1);
+    function test_GetTicketCount() public {
+        console.log("User1 LINK balance:", link.balanceOf(user1));
+        console.log(
+            "Allowance before supply:",
+            link.allowance(user1, address(lottery))
+        );
 
-    //     vm.prank(user1);
+        vm.prank(user1);
+        uint256 firstSupply = lottery.supplyToken(user1, 300);
+        console.log("Tickets issued:", firstSupply);
 
-    //     uint256 firstSupply = lottery.supplyToken(user1, 300);
-    //     console.log("tokens supplied:", firstSupply);
+        uint256[] memory tickets = lottery.getTicketBalance(user1);
+        assertEq(tickets.length, 3);
+        assertEq(tickets[0], 1);
+        assertEq(tickets[1], 2);
+        assertEq(tickets[2], 3);
+    }
+    function test_FailSupplyTokenInsufficientBalance() public {
+        vm.prank(user1);
+        vm.expectRevert(
+            abi.encodeWithSelector(Lottery.InsufficientBalance.selector)
+        );
+        lottery.supplyToken(user1, 2 ether); // More than user1's balance (1 ether)
+    }
 
-    //     uint256[] memory tickets = lottery.getTicketBalance(user1);
-
-    //     assertEq(tickets.length, 3);
-
-    //     assertEq(tickets[0], 1);
-    //     assertEq(tickets[1], 1);
-    //     assertEq(tickets[2], 1);
-    // }
+    function test_Supply0Tokens() public {
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Lottery.AmountZero.selector));
+        lottery.supplyToken(user1, 0);
+    }
 }
